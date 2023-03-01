2a9fe3e420c13a020ce2b06ee178a1967bf130d0
nurannisaamf <100133990+nurannisaamf@users.noreply.github.com>
2023-03-02 06:19:58 +0700

    Add files via upload

---
+++ PRAKTIKUM 1/Titik.java
@@ -0,0 +1,44 @@
+/**
+ * File      : Titik.java		22/02/23
+ * Penulis   : Nur Annisaa' Meiarti Fajrin
+ * Deskripsi : Kelas yang berisi main dari Titik
+ * 
+ */
+
+class Titik{
+	double absis;
+	double ordinat;
+	static int counterTitik;
+	
+	Titik(){
+		absis = 0;
+		ordinat = 0;
+		counterTitik++;
+	}  
+	
+	Titik(double a, double o){
+		absis = a;
+		ordinat = o;
+		counterTitik++;
+	}
+  
+	void setAbsis(double a){
+		absis =a;
+	}
+  
+	double getAbsis(){
+		return absis;
+	}
+	
+	void setOrdinat(double b){
+		ordinat = b;
+	}
+	
+	double getOrdinat(){
+		return ordinat;
+	}
+	
+	int getCounterTitik(){
+		return counterTitik;
+	}
+}
